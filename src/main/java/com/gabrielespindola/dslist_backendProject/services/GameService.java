8503fa295d3990754667e7a45e2d468e967cf298
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-23 17:20:32 -0300

    Find game by id

--- src/main/java/com/gabrielespindola/dslist_backendProject/services/GameService.java
+++ src/main/java/com/gabrielespindola/dslist_backendProject/services/GameService.java
@@ -3,20 +3,29 @@ package com.gabrielespindola.dslist_backendProject.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.gabrielespindola.dslist_backendProject.dto.GameDTO;
 import com.gabrielespindola.dslist_backendProject.dto.GameMinDTO;
 import com.gabrielespindola.dslist_backendProject.entities.Game;
 import com.gabrielespindola.dslist_backendProject.repositories.GameRepository;
 
-@Service
-public class GameService {
 
+ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
+	public GameDTO findById(@PathVariable Long listId) {
+		Game result = gameRepository.findById(listId).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(GameMinDTO::new).toList();
+		
 	}
-}
+ }
