4e268d5be6dbdd0ab78acac2b4eb7880ed46d52a
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-23 00:46:51 -0300

    project created

---
+++ src/main/java/com/gabrielespindola/dslist_backendProject/DslistBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielespindola.dslist_backendProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistBackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielespindola/dslist_backendProject/DslistBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielespindola.dslist_backendProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
