05570afae653cf7dbb0d426e77bd1a12118351a1
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-24 15:16:01 -0300

    Homolog ok

--- src/main/java/com/gabrielespindola/dslist_backendProject/dto/GameMinDTO.java
+++ src/main/java/com/gabrielespindola/dslist_backendProject/dto/GameMinDTO.java
@@ -21,13 +21,29 @@ public class GameMinDTO {
 
 	public GameMinDTO(GameMinProjection projection) {
 		id = projection.getId();
-		title = projection.getTitle();
-		year = projection.getYear();
+		title = projection.getTitle();		
+		year = projection.getGameYear();
 		imgUrl = projection.getImgUrl();
 		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
 		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
 	}
 }
